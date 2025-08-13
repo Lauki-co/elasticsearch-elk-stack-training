beedb75d28f167f4a3699ff1829198e0b938d365
Sumit Sontakke <41416816+sumitsontakke@users.noreply.github.com>
2025-08-13 20:55:23 +0530

    Add files via upload

---
+++ Logstash-config/test_apikey.sh
@@ -0,0 +1,4 @@
+curl -X GET "https://my-elasticsearch-project-a60faa.es.us-central1.gcp.elastic.cloud:443/" \
+  -H "Authorization: ApiKey T3J2X2Y1Z0JoQlZyVDItU3lzcy06UUVkVUR0TC1qZ1lOZ3dMTFBlUDRCdw==" \
+  -H "Content-Type: application/json"
+
